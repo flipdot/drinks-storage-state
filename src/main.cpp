ab16dae70a35a999f6ddb348e03f04b954acb23a
Jonas Platte <jplatte+git@posteo.de>
2017-07-23 00:08:43 +0200

    Working read loop (nodemcuv2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,18 @@
 #include <Arduino.h>
 #include <HX711.h>
 
-constexpr byte CLK_PIN = 1;
-constexpr byte DATA_PIN = 2;
+constexpr byte CLK_PIN = 14;
+constexpr byte DATA_PIN = 12;
 
 HX711 scale(DATA_PIN, CLK_PIN);
 
 void setup() {
-    Serial.begin(9600);
-
-    Serial.println("booted!");
+    //ESP.wdtDisable();
+    Serial.begin(115200);
 }
 
 void loop() {
-    int value = scale.get_value(10);
-    Serial.print(value);
+    int value = scale.get_value();
+    Serial.println(value);
     delay(100);
 }
