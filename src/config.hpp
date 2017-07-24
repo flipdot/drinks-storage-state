4e286c5f2e567fefd31b15f22cb47cc3ae75c09d
Jaro Habiger <jarohabiger@googlemail.com>
2017-07-25 00:41:43 +0200

    add more (untested but building) code

---
+++ src/config.hpp
@@ -0,0 +1,9 @@
+// hardware settings
+constexpr byte CLOCK_PIN = 14;
+constexpr byte DATA_PIN = 12;
+constexpr int DIFFERENCE_THRESHHOLD = 42;
+
+// network settings
+constexpr char * SSID = "kassel.freifunk.net";
+constexpr char * PASSWORD = "";
+constexpr char * HTTP_URL = "http://drinks.fd/collect";
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
 #include <HX711.h>
 
-constexpr byte CLK_PIN = 14;
-constexpr byte DATA_PIN = 12;
+#include <ArduinoJson.h>
 
-HX711 scale(DATA_PIN, CLK_PIN);
+#include "config.hpp"
+
+HX711 * scale;
 
 void setup() {
-    //ESP.wdtDisable();
+    delay(1000);
+
+    // init serial
     Serial.begin(115200);
+    Serial.println("\n\n");
+
+    // init wifi conection
+    Serial.print("connecting to wifi");
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.printf("\nconnected to: %s with IP address %s\n", SSID, WiFi.localIP().toString().c_str());
+
+    // init the scale
+    Serial.println("initializing the HX711...");
+    scale = new HX711(DATA_PIN, CLOCK_PIN);
+
+    Serial.println("setup complete!\n");
 }
 
-void loop() {
-    int value = scale.get_value();
-    Serial.println(value);
-    delay(100);
+int last_measurement = 0;
+void loop(void) {
+    int measurement = scale->read();
+    Serial.println(measurement);
+
+    if(abs(measurement - last_measurement) > DIFFERENCE_THRESHHOLD) {
+        // format json
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& root = jsonBuffer.createObject();
+        root["espId"] = ESP.getChipId();
+        root["scaleValue"] = measurement;
+        String jsonString;
+        root.printTo(jsonString);
+
+        // make http request
+        Serial.printf("\ndoing http request with payload:\n%s\n", jsonString.c_str());
+        HTTPClient http;
+        http.begin(HTTP_URL);
+        int return_code = http.POST(jsonString);
+        Serial.printf("finished http request with return code %d\n\n", return_code);
+    } else {
+        delay(10 * 1000);
+    }
 }
